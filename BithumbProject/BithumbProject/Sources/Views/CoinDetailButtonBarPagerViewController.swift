4dad6f482d0f2d92b09e60a88ca80cc0c45d6ca8
haeseoklee <haeseoklee.dev@gmail.com>
2022-03-03 00:30:37 +0900

    feat: CoinDetailButtonBarPagerViewController 구현

---
+++ BithumbProject/BithumbProject/Sources/Views/CoinDetailButtonBarPagerViewController.swift
@@ -0,0 +1,85 @@
+//
+//  CoinDetailButtonBarPagerViewController.swift
+//  BithumbProject
+//
+//  Created by Haeseok Lee on 2022/03/02.
+//
+
+import Foundation
+
+import Then
+import XLPagerTabStrip
+
+final class CoinDetailButtonBarPagerViewController: ButtonBarPagerTabStripViewController {
+    
+    private let scrollView: UIScrollView = UIScrollView()
+    
+    private let buttonBarPagerView: ButtonBarView = ButtonBarView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewFlowLayout()
+    )
+    
+    private let defaultFont: UIFont = .systemFont(ofSize: 14, weight: .light)
+    private let defaultTextColor: UIColor = .systemGray
+    private let selectedFont: UIFont = .systemFont(ofSize: 14, weight: .medium)
+    private let selectedTextColor: UIColor = .black
+    
+    override func viewDidLoad() {
+        self.setupViews()
+        super.viewDidLoad()
+    }
+    
+    private func setupViews() {
+        
+        settings.style.buttonBarBackgroundColor = .white
+        settings.style.buttonBarItemBackgroundColor = .white
+        settings.style.selectedBarBackgroundColor = .black
+        settings.style.buttonBarItemFont = self.defaultFont
+        settings.style.selectedBarHeight = 2.0
+        settings.style.buttonBarMinimumLineSpacing = 0
+        settings.style.buttonBarItemTitleColor = self.defaultTextColor
+        settings.style.buttonBarItemsShouldFillAvailableWidth = true
+        settings.style.buttonBarLeftContentInset = 0
+        settings.style.buttonBarRightContentInset = 0
+        
+        changeCurrentIndexProgressive = { [weak self] (
+            oldCell: ButtonBarViewCell?,
+            newCell: ButtonBarViewCell?,
+            progressPercentage: CGFloat,
+            changeCurrentIndex: Bool,
+            animated: Bool
+        ) -> Void in
+            guard changeCurrentIndex == true else {
+                return
+            }
+            oldCell?.label.font = self?.defaultFont
+            oldCell?.label.textColor = self?.defaultTextColor
+            newCell?.label.font = self?.selectedFont
+            newCell?.label.textColor = self?.selectedTextColor
+        }
+        
+        self.buttonBarView = self.buttonBarPagerView
+        self.containerView = self.scrollView
+        
+        self.view.addSubview(self.buttonBarPagerView)
+        self.view.addSubview(self.scrollView)
+        
+        buttonBarPagerView.snp.makeConstraints {
+            $0.height.equalTo(40)
+            $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            $0.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leading)
+            $0.trailing.equalTo(self.view.safeAreaLayoutGuide.snp.trailing)
+        }
+        
+        scrollView.snp.makeConstraints {
+            $0.top.equalTo(buttonBarPagerView.snp.bottom)
+            $0.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leading)
+            $0.trailing.equalTo(self.view.safeAreaLayoutGuide.snp.trailing)
+            $0.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
+    
+    override public func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+      return [ChartViewController(), ChartViewController(), ChartViewController(), ChartViewController()]
+    }
+}
