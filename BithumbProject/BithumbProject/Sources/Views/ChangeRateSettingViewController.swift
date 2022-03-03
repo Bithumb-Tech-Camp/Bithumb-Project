3bf76e271c586281cdc4d85d7a034f1fb4b73b06
Paul <phs880623@gmail.com>
2022-03-03 12:52:38 +0900

    feat: 변동률 세팅을 위한 ChageRateSettingViewController 제작
    - 추후에 PanModal로 half present 계획

---
+++ BithumbProject/BithumbProject/Sources/Views/ChangeRateSettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeRateSettingViewController.swift
+//  BithumbProject
+//
+//  Created by 박형석 on 2022/03/03.
+//
+
+import UIKit
+
+class ChangeRateSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemPink
+    }
+
+}
