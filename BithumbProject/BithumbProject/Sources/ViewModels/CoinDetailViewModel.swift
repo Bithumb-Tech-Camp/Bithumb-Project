e95bbdeaa12845e62e373b580b8c09239bf5b912
haeseoklee <haeseoklee.dev@gmail.com>
2022-03-07 21:58:00 +0900

    feat: CoinDetailViewModel 구현

--- BithumbProject/BithumbProject/Sources/ViewModels/CoinDetailViewModel.swift
+++ BithumbProject/BithumbProject/Sources/ViewModels/CoinDetailViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+
 import RxSwift
 import RxRelay
 
@@ -17,6 +18,10 @@ final class CoinDetailViewModel: ViewModelType {
     }
     
     struct Output {
+        let closePriceText: BehaviorRelay<String> = BehaviorRelay<String>(value: "")
+        let changeAmountText: BehaviorRelay<String> = BehaviorRelay<String>(value: "")
+        let changeRateText: BehaviorRelay<String> = BehaviorRelay<String>(value: "")
+        let upDown: BehaviorRelay<UpDown> = BehaviorRelay<UpDown>(value: .up)
         let realtimeClosePriceText: BehaviorRelay<String> = BehaviorRelay<String>(value: "")
         let realtimeChangeAmountText: BehaviorRelay<String> = BehaviorRelay<String>(value: "")
         let realtimeChangeRateText: BehaviorRelay<String> = BehaviorRelay<String>(value: "")
@@ -26,9 +31,53 @@ final class CoinDetailViewModel: ViewModelType {
     
     let input: Input
     let output: Output
+    var disposeBag: DisposeBag = DisposeBag()
     
-    init() {
+    init(orderCurrency: OrderCurrency, httpManager: HTTPManager, webSocketManager: WebSocketManager) {
         self.input = Input()
         self.output = Output()
+        
+        input.fetchTicker
+            .flatMap { _ -> Observable<Ticker> in
+                httpManager.request(httpServiceType: .ticker(orderCurrency), model: Ticker.self)
+            }
+            .subscribe(onNext: { ticker in
+                let changeAmountSign = Double(ticker.fluctate24H ?? "0") ?? 0 >= 0 ? "+" : ""
+                let changeRateSign = Double(ticker.fluctateRate24H ?? "0") ?? 0 >= 0 ? "▲" : "▼"
+                let upDown: UpDown = Double(ticker.fluctate24H ?? "0") ?? 0 >= 0 ? .up : .down
+                
+                self.output.closePriceText.accept(Double(ticker.closingPrice ?? "0")?.decimal ?? "0")
+                self.output.changeAmountText.accept("\(changeAmountSign)\(Double(ticker.fluctate24H ?? "0")?.decimal ?? "0")")
+                self.output.changeRateText.accept("\(changeRateSign)\(Double(ticker.fluctateRate24H ?? "0") ?? 0)%")
+                self.output.upDown.accept(upDown)
+                
+            }, onError: { error in
+                self.output.error.accept(error as NSError)
+            })
+            .disposed(by: disposeBag)
+        
+        input.fetchRealtimeTicker
+            .flatMap { _ -> Observable<RealtimeTicker> in
+                let parameter: [String: Any] = [
+                    "type": BithumbWebSocketRequestType.ticker.rawValue,
+                    "symbols": [Coin(name: orderCurrency).symbol],
+                    "tickTypes": [TickType.thirtyMinute].map { $0.rawValue }
+                ]
+                return webSocketManager.requestRealtime(parameter: parameter, type: RealtimeTicker.self)
+            }
+            .subscribe(onNext: { realtimeTicker in
+                let changeAmountSign = Double(realtimeTicker.changeAmount ?? "0") ?? 0 >= 0 ? "+" : ""
+                let changeRateSign = Double(realtimeTicker.changeRate ?? "0") ?? 0 >= 0 ? "▲" : "▼"
+                let upDown: UpDown = Double(realtimeTicker.changeAmount ?? "0") ?? 0 >= 0 ? .up : .down
+                
+                self.output.realtimeClosePriceText.accept(Double(realtimeTicker.closePrice ?? "0")?.decimal ?? "0")
+                self.output.realtimeChangeAmountText.accept("\(changeAmountSign)\(Double(realtimeTicker.changeAmount ?? "0")?.decimal ?? "0")")
+                self.output.realtimeChangeRateText.accept("\(changeRateSign)\(Double(realtimeTicker.changeRate ?? "0") ?? 0)%")
+                self.output.realtimeUpDown.accept(upDown)
+                
+            }, onError: { error in
+                self.output.error.accept(error as NSError)
+            })
+            .disposed(by: disposeBag)
     }
 }
